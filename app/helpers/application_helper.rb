fa5736fce1090bf167662d1a89e1657c694ed3bf
David Solis <david@soliskit.com>
2016-11-12 23:48:29 -0800

    define method to format date

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  #removed padding for day,month, and hour
+  #added non-breaking space before PM
+  def date_format(utc_date)
+    utc_date.strftime("%-m/%-d/%y %-I:%M\u00A0%p")
+  end
 end
