d4c09f74a5097c3b5c0ea2e26092fd299c8023cd
David Solis <david@soliskit.com>
2016-11-13 16:54:53 -0800

    test redirect to google.com

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -29,7 +29,7 @@ class SubmissionsController < ApplicationController
 
     respond_to do |format|
       if @submission.save
-        # format.html { redirect_to @submission, notice: 'Submission was successfully created.' }
+        format.html { redirect_to "https://www.google.com", notice: 'Submission was successfully created.' }
         format.json { render :show, status: :created, location: @submission }
       else
         format.html { render :new }
