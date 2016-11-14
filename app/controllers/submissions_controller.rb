1eae8351e17eae4c7c7e7cbaaa49b272f3f7253c
David Solis <david@soliskit.com>
2016-11-13 16:31:28 -0800

    allow Submission Controller to skip csrf check

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -1,4 +1,5 @@
 class SubmissionsController < ApplicationController
+  skip_before_action :verify_authenticity_token 
   before_action :set_submission, only: [:show, :edit, :update, :destroy]
 
   # GET /submissions
