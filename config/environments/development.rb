f6b959ef70462d77cc6a9709036ec21e5cafbeea
David Solis <david@soliskit.com>
2016-12-04 13:47:34 -0800

    config local mail delivery

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  Rails.application.routes.default_url_options[:host] = 'localhost:5000'
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
@@ -26,8 +27,12 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  # Ensure mailer works in dev environment.
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = {:host => 'localhost:5000'}
+  config.action_mailer.asset_host = "http://localhost:5000"
 
   config.action_mailer.perform_caching = false
 
