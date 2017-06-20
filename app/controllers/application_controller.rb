aa8cb100af07aa09b21667083e279afc2dcc6bca
David Solis <david@soliskit.com>
2017-06-20 11:10:16 -0700

    Refactor

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,13 @@ class ApplicationController < ActionController::Base
   # allow_cors takes in arbitrarily many symbols representing actions that
   # CORS should be enabled for
   def self.allow_cors(*methods)
-    before_action :cors_before_filter, :only => methods
+    before_action :cors_filter, only: :methods
 
     # Rails recommends to use :null_session for APIs
-    protect_from_forgery with: :null_session, :only => methods
+    protect_from_forgery with: :null_session, only: :methods
   end
 
-  def cors_before_filter
+  def cors_filter
     # Check that the `Origin` field matches our front-end client host
     if is_approved_domain?
       headers['Access-Control-Allow-Origin'] = request.headers['Origin']
@@ -16,6 +16,6 @@ class ApplicationController < ActionController::Base
   end
 
   def is_approved_domain?
-    request.headers['Origin'] == "http://davidmazza.com"
+    request.headers['Origin'] == "http://davidsolis.me" || "http://davidmazza.com"
   end
 end
