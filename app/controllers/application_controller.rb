cc6c58c715933566a2be2895bfe6820373cab773
David Solis <david@soliskit.com>
2017-06-26 23:45:21 -0700

    authenticate except for api calls

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!, except: :create
   # Prevent CSRF attacks by raising an exception.
   protect_from_forgery with: :exception, except: :create
 
