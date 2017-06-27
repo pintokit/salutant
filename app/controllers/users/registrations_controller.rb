ea974d997f69bebe6403ee2e4d36d0e2b140601f
David Solis <david@soliskit.com>
2017-06-27 01:06:27 -0700

    Remove signup action

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,47 @@
+class Users::RegistrationsController < Devise::RegistrationsController
+  # before_action :configure_sign_up_params, only: [:create]
+  # before_action :configure_account_update_params, only: [:update]
+
+  # GET /users/sign_up
+  # def new
+  #   super
+  # end
+
+  # POST /users
+  def create
+    super
+  end
+
+  # GET /users/edit
+  def edit
+    super
+  end
+
+  # PUT /users
+  def update
+    super
+  end
+
+  # DELETE /users
+  def destroy
+    super
+  end
+
+  protected
+
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_sign_up_params
+  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
+  # end
+
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_account_update_params
+  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
+  # end
+
+  # The path used after sign up.
+  def after_sign_up_path_for(resource)
+    super(resource)
+  end
+
+end
