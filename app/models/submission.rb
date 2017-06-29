487673a4122740a92d0d73eb41bbbce34b8a954f
David Solis <david@soliskit.com>
2017-06-29 13:54:38 -0700

    Store Origin Header

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,6 +1,6 @@
 class Submission < ApplicationRecord
   serialize :content
-  store_accessor :content, :body, :phone
+  store_accessor :content, :sent_to, :body, :phone
 
   enum filter_result: [:discard, :spam, :not_spam]
 
