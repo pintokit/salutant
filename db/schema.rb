340ea01493f387d8d3faeb0995ae43114c3400a2
David Solis <litlmoz@gmail.com>
2017-08-10 17:30:10 -0700

    Add is unread attribute, default to true

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170627053428) do
+ActiveRecord::Schema.define(version: 20170811000712) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20170627053428) do
     t.datetime "updated_at", null: false
     t.integer "filter_result"
     t.jsonb "headers"
+    t.boolean "is_unread", default: true
   end
 
   create_table "users", force: :cascade do |t|
