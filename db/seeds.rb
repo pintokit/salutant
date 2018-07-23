0df4a60df8be521e42666932cb309d444fb4d869
David Solis <litlmoz@gmail.com>
2018-07-23 16:36:18 -0700

    update admin pw variable

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,4 +2,4 @@ puts "Seeding admin users..."
 
 User.create(email: ENV['ADMIN_EMAIL'], password: ENV['ADMIN_PW'], confirmed_at: Time.now)
 
-User.create(email: ENV['ADMIN_2_EMAIL'], password: ENV['ADMIN_2_PW'], confirmed_at: Time.now)
+User.create(email: ENV['ADMIN_2_EMAIL'], password: ENV['ADMIN_PW'], confirmed_at: Time.now)
